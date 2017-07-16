f2018a5a175eb7de2a95f8d9a43ffb4e55b19c3f
Stanley Su <stanleysu00@gmail.com>
2017-07-16 15:21:43 +1000

    added search functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+let search = document.getElementById('search'),
+	results = document.getElementById('results');
+
+search.addEventListener('keyup', function(e) {
+	if (e.which === 13 && e.target.value) {
+		getQuery(e.target.value);
+	}
+});
+
+function getQuery(query) {
+	let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&limit=5&search=' + encodeURIComponent(query) + '&callback=?';
+	$.getJSON(url, function(data) {
+	  loadData(data);
+	});
+}
+
+function loadData(data) {
+	let titles = data[1],
+		summaries = data[2],
+		links = data[3];
+
+	for (let i = 0, len = titles.length; i < len; ++i) {
+		let a = document.createElement('a');
+		a.setAttribute('href', links[i]);
+		a.textContent = titles[i] + summaries[i];
+		results.appendChild(a);
+	}
+}

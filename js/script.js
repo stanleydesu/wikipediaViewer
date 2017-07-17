58bc6bf744afaa3f2c88dce9a062e5a883c719c4
Stanley Su <stanleysu00@gmail.com>
2017-07-17 13:05:43 +1000

    updated formatting

--- js/script.js
+++ js/script.js
@@ -1,51 +1,51 @@
 (function() {
-	"use strict";
+  "use strict";
 
-	let wrapper = document.querySelector('.wrapper'),
-		search = document.getElementById('search'),
-		results = document.getElementById('results'),
-		error = document.getElementById('error');
+  let wrapper = document.querySelector('.wrapper'),
+      search = document.getElementById('search'),
+      results = document.getElementById('results'),
+      error = document.getElementById('error');
 
-	search.addEventListener('keyup', function(e) {
-		if (e.target.value && e.which === 13) {
-			// clear list
-			results.innerHTML = "";
-			// add back error message
-			results.appendChild(error);
-			// hide error
-			error.style.display = "none";
-			// move wrapper to top
-			wrapper.style.top = '3em';
-			// search
-			getQuery(e.target.value);
-		}
-	});
+  search.addEventListener('keyup', function(e) {
+    if (e.target.value && e.which === 13) {
+      // clear list
+      results.innerHTML = "";
+      // add back error message
+      results.appendChild(error);
+      // hide error
+      error.style.display = "none";
+      // move wrapper to top
+      wrapper.style.top = '3em';
+      // search
+      getQuery(e.target.value);
+    }
+  });
 
-	search.addEventListener('blur', function() {
-		search.value = "";
-	});
+  search.addEventListener('blur', function() {
+    search.value = "";
+  });
 
-	function getQuery(query) {
-		let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&search=' + encodeURIComponent(query) + '&callback=?';
-		$.getJSON(url, function(data) {
-		  loadData(data);
-		});
-	}
+  function getQuery(query) {
+    let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&search=' + encodeURIComponent(query) + '&callback=?';
+    $.getJSON(url, function(data) {
+      loadData(data);
+    });
+  }
 
-	function loadData(data) {
-		let titles = data[1],
-			summaries = data[2],
-			links = data[3];
+  function loadData(data) {
+    let titles = data[1],
+        summaries = data[2],
+        links = data[3];
 
-		console.log(titles.length);
-		// if no response
-		if (titles.length == 0) {
-			// show error message
-			error.style.display = "block";
-		} else {
-			for (let i = 0, len = titles.length; i < len; ++i) {
-				results.innerHTML += `<a href="${links[i]}"><div class="result"><h1>${titles[i]}</h1><p>${summaries[i]}</p></div></a>`;
-			}
-		}
-	}
+    console.log(titles.length);
+    // if no response
+    if (titles.length == 0) {
+      // show error message
+      error.style.display = "block";
+    } else {
+      for (let i = 0, len = titles.length; i < len; ++i) {
+        results.innerHTML += `<a href="${links[i]}"><div class="result"><h1>${titles[i]}</h1><p>${summaries[i]}</p></div></a>`;
+      }
+    }
+  }
 })();

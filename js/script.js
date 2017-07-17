9470dbd2b16e9001e9ec88aad0dbd15070e48b84
Stanley Su <stanleysu00@gmail.com>
2017-07-17 12:27:08 +1000

    updated dom manipulation

--- js/script.js
+++ js/script.js
@@ -1,35 +1,39 @@
-"use strict";
+(function() {
+	"use strict";
 
-let wrapper = document.querySelector('.wrapper'),
-	search = document.getElementById('search'),
-	results = document.getElementById('results');
+	let wrapper = document.querySelector('.wrapper'),
+		search = document.getElementById('search'),
+		results = document.getElementById('results');
 
-search.addEventListener('keyup', function(e) {
-	if (e.which === 13 && e.target.value) {
-		// move the search box up
-		getQuery(e.target.value);
-	}
-});
-
-function getQuery(query) {
-	let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&search=' + encodeURIComponent(query) + '&callback=?';
-	$.getJSON(url, function(data) {
-	  loadData(data);
+	search.addEventListener('keyup', function(e) {
+		if (e.target.value && e.which === 13) {
+			// clear list
+			results.innerHTML = "";
+			// move wrapper to top
+			wrapper.style.top = '3em';
+			// search
+			getQuery(e.target.value);
+		}
 	});
-}
 
-function loadData(data) {
-	let titles = data[1],
-		summaries = data[2],
-		links = data[3];
-	/*
+	search.addEventListener('blur', function(e) {
+		e.target.value = "";
+	});
 
-	div hre
-		h1
-		summary
-	*/
-
-	for (let i = 0, len = titles.length; i < len; ++i) {
-		results.innerHTML += `<a href="${links[i]}"><div class="result"><h1>${titles[i]}</h1><p>${summaries[i]}</p></div></a>`;
+	function getQuery(query) {
+		let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&search=' + encodeURIComponent(query) + '&callback=?';
+		$.getJSON(url, function(data) {
+		  loadData(data);
+		});
 	}
-}
+
+	function loadData(data) {
+		let titles = data[1],
+			summaries = data[2],
+			links = data[3];
+
+		for (let i = 0, len = titles.length; i < len; ++i) {
+			results.innerHTML += `<a href="${links[i]}"><div class="result"><h1>${titles[i]}</h1><p>${summaries[i]}</p></div></a>`;
+		}
+	}
+})();
